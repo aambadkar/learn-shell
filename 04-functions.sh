35b7ecc5842b6eab9f52fc72faec7c09b67ff316
aambadkar <khodaskar.ankita@gmail.com>
2023-09-11 17:33:17 +0530

    expense script

--- 04-functions.sh
+++ 04-functions.sh
@@ -1,6 +1,10 @@
+if we assign a name to command , then it is a function(it is a bunch of command)
+
 # declear a function
 abc() {
   echo hello world
+  echo hello Universe
+  echo hello galaxy
 
 }
 
